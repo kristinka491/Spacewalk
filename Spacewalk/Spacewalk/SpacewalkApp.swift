ec2b06a3837d6bf852a1074118cdeba047d5f6a9
Krystsina Suvorava <>
2023-02-17 12:45:06 +0200

    Project was created

---
+++ Spacewalk/Spacewalk/SpacewalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacewalkApp.swift
+//  Spacewalk
+//
+//  Created by Krystsina on 2023-02-17.
+//
+
+import SwiftUI
+
+@main
+struct SpacewalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
